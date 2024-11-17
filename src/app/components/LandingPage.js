98c82bb4d0fe38cbfafd6859fcf34f7152ca128e
Yash Sen <132945201+yaashhhh13@users.noreply.github.com>
2024-11-17 14:48:03 -0500

    project setup done

---
+++ src/app/components/LandingPage.js
@@ -0,0 +1,10 @@
+import Navbar from "./Navbar";
+
+export default function LandingPage() {
+    return (
+        <div className="w-full">
+            <Navbar/>
+            <h1 className="text-8xl leading-none">Landing Page</h1>
+        </div>
+    )
+}
---
+++ src/app/components/Navbar.js
@@ -0,0 +1,15 @@
+export default function Navbar() {
+    return (
+        <div className="w-full p-4 flex items-center justify-between text-[#0A5029]">
+            <div>
+                <h1>Yash Sen</h1>
+            </div>
+            <div className="p-4 flex items-center gap-4">
+                <h1>About</h1>
+                <h1>Projects</h1>
+                <h1>Resume</h1>
+                <h1>Contact</h1>
+            </div>
+        </div>
+    )
+}
